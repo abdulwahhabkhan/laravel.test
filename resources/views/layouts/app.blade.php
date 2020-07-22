3c67094467f161b85ceafb682e1c3210e5775224
Abdul Wahhab Khan <abdul.webequator.com>
2020-07-22 23:10:24 +0100

    style updated for header and logo added

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -21,10 +21,10 @@
 </head>
 <body>
     <div id="app">
-        <nav class="navbar navbar-expand-md navbar-dark bg-dark shadow-sm">
+        <nav class="navbar navbar-expand-md navbar-dark bg-dark shadow-sm main-menu">
             <div class="container-fluid">
                 <a class="navbar-brand" href="{{ url('/') }}">
-                    {{ config('app.name', 'Laravel') }}
+                    <img src="{{ asset('/img/logo.png') }}" alt="{{ config('app.name', 'Laravel') }}" style="max-height: 30px;">
                 </a>
                 <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
                     <span class="navbar-toggler-icon"></span>
